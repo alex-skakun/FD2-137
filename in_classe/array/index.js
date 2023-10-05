21cb5ad58c7cde52a85cbcb1caefebc1b81f672d
S-Kovrik <sergey.covrik@gmail.com>
2023-10-05 21:50:05 +0300

    array

---
+++ in_classe/array/index.js
@@ -0,0 +1,24 @@
+"use strict";
+
+// function collectRepeat(str){
+
+// // const a1 = str.split('');
+// // const a2 = [...str];
+
+// return [...str].reduce((stat, char) => stat[symbol] = (stat[symbol] ?? 0) + 1;
+
+// return stat;
+// }, {});
+// }
+
+function a (...numb){
+    const number = numb.filter(function(numb){
+        if(numb % 2 === 0){
+            return number;
+        }
+    });
+    return numb;
+}
+
+a ([1,2,3,4,5]);
+
