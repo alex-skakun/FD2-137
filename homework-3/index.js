101af5dd2762f8487eb6c977914d6814ee862f1c
Aliaksei <43481213+aliakseiMaiseyeu@users.noreply.github.com>
2023-10-09 22:03:41 +0300

    home work 3 Alexei Moiseev

---
+++ homework-3/index.js
@@ -0,0 +1,80 @@
+const columns = ['name', 'count', 'price'];
+const data = [
+  { name: 'Хлеб', count: 12, price: 14.99 },
+  { name: 'Молоко', count: 3, price: 3.2 },
+  { name: 'Сыр', count: 1, price: 10 },
+  { name: 'Вода', count: 2, price: 5.5 },
+  { name: 'Вода', count: 2, price: 5.5 },
+];
+
+const horizontalLine = '─';
+const verticalLine = '│';
+const topLeftCorner = '┌';
+const topRightCorner = '┐';
+const bottomLeftCorner = '└';
+const bottomRightCorner = '┘';
+const leftMop = '├';
+const rightMop = '┤';
+const cross = '┼';
+let maxLengthStr;
+let lengthIteratorStr;
+
+console.log((textTable = `${topLeftCorner}${horizontalLine.repeat(22)}${topRightCorner}`));
+for (const iterator of data) {
+  console.log(
+    `${verticalLine} ${iterator.name}   ${verticalLine} ${iterator.count} ${verticalLine} ${
+      iterator.price
+    } ${verticalLine}\n${leftMop}${horizontalLine.repeat(22)}${rightMop}`
+  );
+}
+console.log(`${bottomLeftCorner}${horizontalLine.repeat(22)}${bottomRightCorner} `);
+
+// закоментировал попытку сделать нормально, было мало времени и нерешил затык с отниманием
+
+// получаю максимальную строку
+
+// function getMaxStringLength(dataLength) {
+//   let arrNumb = [];
+//   for (const iterator of dataLength) {
+//     lengthIteratorStr = iterator.name.length;
+//     arrNumb.push(lengthIteratorStr);
+//   }
+
+//   maxLengthStr = Math.max.apply(null, arrNumb);
+//   return maxLengthStr;
+// }
+// getMaxStringLength(data);
+
+// добавляю пробелы
+
+// function addSpaceInStr(data) {
+//   let textTable = '';
+
+//   let addSpace;
+//   let lengthIterationStr;
+//   let spaceСount = 0;
+//   for (const iterator of data) {
+//     lengthIterationStr = iterator.name.length;
+
+//     if (maxLengthStr === lengthIterationStr) {
+//       addSpace = iterator.name + '1';
+//     } else {
+//       spaceСount = Number(maxLengthStr) - Number(lengthIteratorStr);  /*  тут spaceСount выдавал одно и тоже значение на каждой итерации   */
+//       console.log(spaceСount);
+
+//       console.log(maxLengthStr);
+//       console.log(lengthIterationStr);
+
+//       addSpace = `${iterator.name} ${'1'.repeat(spaceСount)}`;
+//     }
+//     console.log(addSpace);
+//   }
+// }
+// addSpaceInStr(data);
+
+// function createTextTable(columns, data) {
+
+//   return textTable;
+// }
+
+// createTextTable(columns, data);
