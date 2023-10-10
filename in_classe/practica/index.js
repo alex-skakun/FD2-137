95ee91eaf448a86c8e0f9508cc21993f96a394ff
S-Kovrik <sergey.covrik@gmail.com>
2023-10-10 21:44:58 +0300

    asdfas

---
+++ in_classe/practica/index.js
@@ -0,0 +1,55 @@
+"use strict";
+
+// function task1(i) {
+
+//   for (let i = 1; i <= 50; i++){
+//       console.log(i)
+//   }
+// }
+// task1()
+
+// function arr (arr1){
+//   for (let i = 0; i < arr1.length; i++){
+//       console.log(arr1[i]);
+//   }
+// }
+// arr([1,2,3,4,5,6]);
+
+// function func (arr){
+//   let res = 0;
+//   for (let i = 0; i < arr.length; i++){
+//       res *= arr[i];
+//   }
+//   return res;
+// }
+// func([1,2,3,4,5,6,7]);
+
+// function func(arr) {
+//   let res = arr.filter(function (el, arr) {
+//     return el >= 0 && el <= 100 && el % 2 === 0;
+//   });
+//   return res;
+// }
+
+// function func (numb){
+//   let i = 0;
+//   let res;
+//     while(res = numb/2 && numb >= 50){
+//   i++;
+//   }
+  
+//   return i;
+      
+//    }
+   
+//    func(1000);
+
+// function func (arr){
+
+//   let res = arr.reduce(function(el, current) {
+//     return el + current;
+//   }, 0);
+//   return res;
+//   }
+//   func(3046);
+  
