2adc8d3412a92f7ea34ad42b5e047fabaefca314
Aleksandr Gubich <a.gubich@bk.ru>
2023-10-24 12:28:36 +0300

    Исправлена ошибка вывода слов в hw5

--- homework-5/index.js
+++ homework-5/index.js
@@ -1,17 +1,16 @@
 function printWithDelay(stringToSpell, delayArray) {
     const stringToWords = stringToSpell.split(' ');
-    const maxDelays = Math.min(stringToWords.length, delayArray.length);
 
     function printWord(index) {
-        if (index < maxDelays) {
+        if (index < stringToWords.length) {
             setTimeout(() => {
                 console.log(stringToWords[index]);
                 printWord(index + 1);
-            }, delayArray[index] * 1000);
+            }, (index < delayArray.length ? delayArray[index] : delayArray[delayArray.length - 1]) * 1000);
         }
     }
 
     printWord(0);
 }
 
-printWithDelay('Мама мыла раму', [2, 6, 7]);
+printWithDelay('Мама мыла раму или не мыла точно не уверен', [2, 6, 7]);
