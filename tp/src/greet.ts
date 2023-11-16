e620fc48b57d84df7506806892d62e15560c93b8
S-Kovrik <sergey.covrik@gmail.com>
2023-11-16 21:49:13 +0300

    DOM_1.0

--- tp/src/greet.ts
+++
@@ -1,3 +0,0 @@
-export function greet(message: string): void {
-  alert(message);
-}
--- tp/src/index.ts
+++ tp/src/index.ts
@@ -1,3 +1,21 @@
-import { greet } from "./greet";
+const mainEl = document.getElementById("main");
+const spanCollection = mainEl
+  ? Array.from(mainEl.querySelectorAll<HTMLElement>("[data-color]"))
+  : [];
 
-greet("Hello World");
+spanCollection.forEach((spanEl) => {
+  const colorValue = spanEl.textContent;
+  if (colorValue) {
+    const painBg = () => {
+      spanEl.style.setProperty("background-color", colorValue);
+
+      setTimeout(() => {
+        spanEl.style.removeProperty("background-color");
+      }, 1_000);
+    };
+    spanEl.addEventListener("click", painBg);
+    setTimeout(() => {
+      spanEl.removeEventListener("click", painBg);
+    }, 5_000);
+  }
+});
