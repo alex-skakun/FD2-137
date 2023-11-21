64de8ffa27c7598c9befa0262fb0cd47b3c25746
S-Kovrik <sergey.covrik@gmail.com>
2023-11-21 21:56:11 +0300

    DOM_1.1

--- tp/src/index.ts
+++ tp/src/index.ts
@@ -1,21 +1,47 @@
+import { COLORS } from "./colors";
+import { createElementByColor } from "./createElementByColor";
+
 const mainEl = document.getElementById("main");
-const spanCollection = mainEl
-  ? Array.from(mainEl.querySelectorAll<HTMLElement>("[data-color]"))
-  : [];
+// const spanCollection = mainEl
+//   ? Array.from(mainEl.querySelectorAll<HTMLElement>("[data-color]"))
+//   : [];
 
-spanCollection.forEach((spanEl) => {
-  const colorValue = spanEl.textContent;
-  if (colorValue) {
-    const painBg = () => {
-      spanEl.style.setProperty("background-color", colorValue);
+// spanCollection.forEach((spanEl) => {
+//   const colorValue = spanEl.textContent;
+//   if (colorValue) {
+//     const painBg = () => {
+//       spanEl.style.setProperty("background-color", colorValue);
+//     };
+//     spanEl.addEventListener("click", painBg);
+//   }
+// });
 
-      setTimeout(() => {
-        spanEl.style.removeProperty("background-color");
-      }, 1_000);
-    };
-    spanEl.addEventListener("click", painBg);
-    setTimeout(() => {
-      spanEl.removeEventListener("click", painBg);
-    }, 5_000);
+const fragment = createDocumentFragment();
+
+COLORS.forEach((color) => {
+  fragment.appendChild(createElementByColor(color));
+});
+
+mainEl?.appendChild(fragment);
+
+const coloredElements = COLORS.map((color) => {
+  createElementByColor(color);
+});
+
+// for (const coloredElement of coloredElements){
+// mainEl?.appendChild(coloredElement);
+// }
+
+// mainEl?.append(...coloredElements);
+// mainEl?.insertAdjacentElement('beforebegin');
+
+mainEl?.addEventListener("click", (event) => {
+  const targetEl = event.target as HTMLElement;
+  const coloredEl = targetEl.closest<HTMLElement>("data-color");
+
+  if (coloredEl) {
+    const colorValue = coloredEl.getAttribute("data-color");
+
+    coloredEl.style.setProperty("background-color", colorValue);
   }
 });
