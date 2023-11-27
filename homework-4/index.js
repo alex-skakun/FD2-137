ca593447e327fb5de44036f9c5aeb6ede35ec4e9
EvgeniyPuca <epuca3285@gmail.com>
2023-11-27 19:55:27 +0300

    commit

--- homework-4/index.js
+++ homework-4/index.js
@@ -1,67 +1,123 @@
+// class Validator {
+//     constructor (arrValidators, modification = {mode: 'single'}) {
+//         this.validators = arrValidators;
+//         this.modification = modification;
+//         this.enabled = true;
+//     }
+//     enable() {
+//         this.enabled = true;
+//     }
+//     disable() {
+//         this.enabled = false;
+//     }
+//     toggle(value) {
+//         if (value !== undefined) {
+//             this.enabled = value;
+//         } else {
+//             this.enabled = !this.enabled;
+//         }
+//     }
+
+//     validate(value) {
+//         if (!this.enabled) {
+//             return null;
+//         }
+//         let result = {};
+
+//         for (let elem of this.validators) {
+//             if (elem(value) !== null) {
+//                 result = { ...result, ...elem(value) };
+//                 if (this.modification.mode === 'single') {
+//                     break;
+//                 }
+//             }
+//         }
+
+//         if (Object.keys(result).length === 0) {
+//             return null;
+//         } else {
+//             return result;
+//         }
+//     } 
+// }
+//---------------------------------------------------------
+
 class Validator {
-    constructor (arrValidators, modification = {mode: 'single'}) {
-        this.validators = arrValidators;
+    #enabled = true;
+    
+
+    constructor(functions, modification = { mode: 'single' }) {
         this.modification = modification;
-        this.enabled = true;
+        this.arrFunctions = functions;
+     }
+    get enabled() {
+        return this.#enabled;
     }
+
     enable() {
-        this.enabled = true;
+        this.#enabled = true;
     }
     disable() {
-        this.enabled = false;
+        this.#enabled = false;
     }
-    toggle(value) {
-        if (value !== undefined) {
-            this.enabled = value;
-        } else {
-            this.enabled = !this.enabled;
-        }
+    toggle(stat = !this.#enabled) {
+        this.#enabled = stat;
     }
+ 
 
     validate(value) {
-        if (!this.enabled) {
+        if (this.enable === false) {
             return null;
         }
-        let result = {};
 
-        for (let elem of this.validators) {
-            if (elem(value) !== null) {
-                result = { ...result, ...elem(value) };
-                if (this.modification.mode === 'single') {
-                    break;
-                }
+        switch (this.modification.mode) {
+            case 'multi':return this.multifunction(value);
+            
+            case 'single':return this.singlefunction(value);
+
+            default:
+        }
+    }
+    singlefunction (value) {
+        for (let elem of this.arrFunctions){
+            if (elem(value)){
+                return elem(value);
             }
+            return null;   
         }
-        
-        if (Object.keys(result).length === 0) {
-            return null;
-        } else {
-            return result;
-        }
-    } 
+    }
+    multifunction(value) {
+       const errors = this.arrFunctions.reduce((acc, elem) => {
+            if (elem(value)){
+                acc.push(elem(value));
+            } return acc;
+       }, []);
+       return errors.length ? Object.assign({}, ...errors) : null;
+    }
 }
 
+
 const required = (value) => {
-    return Boolean(value) ? null : {required: true};
+    return Boolean(value) ? null : { required: true };
 };
 const minLength = (minLength) => {
     return (value) => {
-        return String(value).length >= minLength ? null : {minLength: true};
+        return String(value).length >= minLength ? null : { minLength: true };
     };
 };
 const maxLength = (maxLength) => {
     return (value) => {
-        return String(value).length <= maxLength ? null : {maxLength: true};
+        return String(value).length <= maxLength ? null : { maxLength: true };
     };
 };
 const min = (min) => {
     return (value) => {
-        return value >= min ? null : {min: true};
+        return value >= min ? null : { min: true };
     };
 };
 const max = (max) => {
     return (value) => {
-        return value <= max ? null : {max: true};
+        return value <= max ? null : { max: true };
     };
 };
 
@@ -71,17 +127,17 @@ const validator = new Validator([
     maxLength(25),
 ]);
 
-  console.log(validator.validate('test')); // {minLength: true}
-  console.log(validator.validate('')); // {required: true}
-  console.log(validator.validate('successful')); // null
+console.log(validator.validate('test')); // {minLength: true}
+console.log(validator.validate('')); // {required: true}
+console.log(validator.validate('successful')); // null
 
-  const multiValidator = new Validator([
-        required,
-        minLength(5),
-        maxLength(25),
-        min ('111110'),
-        max ('111113')
-      ], {mode: 'multi'});
-      
-      console.log(multiValidator.validate(null)); 
-    
+const multiValidator = new Validator([
+    required,
+    minLength(5),
+    maxLength(25),
+    min('111110'),
+    max('111113')
+], { mode: 'multi' });
+
+console.log(multiValidator.validate(null));
+
