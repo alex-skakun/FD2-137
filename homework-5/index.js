829eb67d70941674536b134db7d64cb54840bb7f
Екатерина Солдатенко <sergey.covrik@gmail.com>
2023-11-28 17:08:42 +0300

    hk5_1.3

--- homework-5/index.js
+++ homework-5/index.js
@@ -1,42 +1,16 @@
 "use strict";
 
-setTimeout(() => {
-  clearTimeout(pullWord());
-}, 2_0000);
-
-setInterval(
-  () =>
-    function pullWord(from) {
-      const arrlistOfWords = from.split(" ");
-
-      return arrlistOfWords.shift(el);
-    },
-  3_000
-);
-
-setTimeout(() => {
-  clearTimeout(getTimeDelay());
-}, 3_0000);
-
-setInterval(
-  () =>
-    function getTimeDelay(to) {
-      return to.shift(el);
-    },
-  3_000
-);
+printWithDelay("Мама мыла раму", [2, 6, 7]);
 
 function printWithDelay(from, to) {
-  setTimeout(() => {
-    const timerId = setInterval(
-      () => console.log(pullWord(from)),
-      getTimeDelay(to) * 1_000
-    );
 
-    setTimeout(() => {
-      clearTimeout(timerId);
-    }, 1_0000);
-  }, 1_000);
+  setInterval(() => {
+    const listOfWords = from.split(" ");
+    console.log(listOfWords[0]);
+    listOfWords.reverse().pop();
+    if (to.length <= from.length){
+      to.reverse().pop();
+      printWithDelay(listOfWords.join(" "));
+    }
+  }, to * 1000);
 }
-
-printWithDelay("Мама мыла раму", [2, 6, 7]);
