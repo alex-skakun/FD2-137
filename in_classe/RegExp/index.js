deddb5422a8c3dd134badad59108bc2bf2da04f0
S-Kovrik <sergey.covrik@gmail.com>
2023-11-30 20:23:18 +0300

    practica rexExp

--- in_classe/RegExp/index.js
+++ in_classe/RegExp/index.js
@@ -4,16 +4,32 @@
 //   });
 // }
 
-function toCamelCase1(text) {
-  return text.replace(/_[a-z]/gi, (match) => match.chartAt(1).toUpperCase());
-}
+// function toCamelCase1(text) {
+//   return text.replace(/_[a-z]/gi, (match) => match.chartAt(1).toUpperCase());
+// }
 
-function toCamelCase2(text) {
-  return text.replace(/_([a-z])/gi, (_, letter) => letter.toUpperCase());
-}
+// function toCamelCase2(text) {
+//   return text.replace(/_([a-z])/gi, (_, letter) => letter.toUpperCase());
+// }
 
-function toCamelCase3(text) {
-  return text.replace(/_(?<letter>[a-z])/gi, (...args) =>
-    args.at(-1).letter.toUpperCase()
-  );
+// function toCamelCase3(text) {
+//   return text.replace(/_(?<letter>[a-z])/gi, (...args) =>
+//     args.at(-1).letter.toUpperCase()
+//   );
+// }
+
+function t(text: String, data: Record<String, unknown>): string {
+  return text.replace(/{\{(\w+)}\}/g, (match, key) => {
+    if (Object.hasOwn(data, key)) {
+      return String(data[key]);
+    } else {
+      return match;
+    }
+  });
 }
+function t(text, data) {
+  return text.replace(/{\{(\w+)}\}/g, (match, key) => {
+    return Object.hasOwn(data, key) ? String(data[key]) : match;
+  });
+}
+t("у вас {{count}} товаров в корзине", { count: 6 });
