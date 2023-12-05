1a7c67dc47b7d6d01d78997d64bf5d859f98a964
S-Kovrik <sergey.covrik@gmail.com>
2023-12-05 21:50:29 +0300

    DOM_practica_void

--- tp/src/FormValidatorConfiguration.ts
+++
@@ -1,5 +0,0 @@
-import { ValidatorFunction } from "./ValidatorFunction";
-
-export type FormValodatorConfiguration<Data extends object> = {
-  [Property in keyof Data]?: ValidatorFunction<Data[Property]>[];
-};
