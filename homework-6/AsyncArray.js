eb22292756a96f697b1628c38c4d7db266232caf
S-Kovrik <sergey.covrik@gmail.com>
2023-12-13 18:20:13 +0300

    jb

--- homework-6/AsyncArray.js
+++ homework-6/AsyncArray.js
@@ -1,25 +1,42 @@
 "use strict";
 
-import Array from "./Array";
+// class Array {
+//   value;
 
-new Array();
+//   constructor(value) {
+//     this.value = value;
+//   }
+
+//   hello() {
+//     alert(this.value);
+//   }
+// }
+
+// new Array();
 
 class AsyncArray extends Array {
-  #coolIndex;
-
-  constructor(value, coolIndex) {
-    super(value);
-    this.#coolIndex = coolIndex;
+  constructor(asyncStr) {
+    super(asyncStr);
   }
 
-  hello() {
-    console.log(this.value);
-    this.#bye();
-  }
-
-  #bye() {
-    console.log(this.#coolIndex);
-  }
+  serialMap() {}
 }
 
 const asyncArray = new AsyncArray(1, 2, 3);
+
+const asyncTransformation = (asynsStr) => {
+  const asynsArr = asynsStr.split();
+
+  new Promise((resolve) => {
+    setTimeout(() => resolve(el), 1000);
+  });
+
+  // любое асинхронное преобразование
+};
+
+asyncArray.serialMap(asyncTransformation).then((result) => {
+  return (result = asynsArr.map((el) => {
+    el + 1;
+  }));
+  // result - экземпляр AsyncArray с результатами преобразований
+});
